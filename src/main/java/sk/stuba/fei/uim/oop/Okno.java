f2734cf7ba4a4b56a87668e862331fcceabe468c
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-29 14:09:21 +0200

    lambda action listener

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -12,6 +12,8 @@ public class Okno extends JFrame {
         this.setVisible(true);
         this.setSize(300,300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.addMouseListener(new MousePositionLogger());
+        var btn = new JButton("button");
+        this.add(btn);
+        btn.addActionListener(e -> System.out.println("click"));
     }
 }
