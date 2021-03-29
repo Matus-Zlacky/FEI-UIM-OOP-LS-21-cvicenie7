558b32fadaa31493881cbd0a579ad41b8e9d15da
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-29 13:37:17 +0200

    hello deleted repo 1

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -8,8 +8,4 @@ public class Main {
     protected void ahoj() {
         System.out.println("ahoj svet");
     }
-
-    protected void hello() {
-        System.out.println("hello world");
-    }
 }
