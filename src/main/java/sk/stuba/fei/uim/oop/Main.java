5dd0d7fe091d6fc101674a8c17e19b70c8dac599
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-29 14:02:37 +0200

    default close operation

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -7,15 +7,7 @@ public class Main {
     private final static String GREETING = "Ahoj svet";
 
     public static void main(String[] args) {
-        Clovek c = new Clovek();
-        c.setAdresa("adresa");
-        c.setMeno("meno");
-        c.setVaha(80.0);
-        c.setVyska(180.0);
-        c.setVek(20);
-        c.setKamarati(List.of(new Clovek()));
-
-        System.out.println(c.toString());
+        new Okno();
     }
 
     protected void ahoj() {
